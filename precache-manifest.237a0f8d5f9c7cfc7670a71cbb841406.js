70cc196e348bed7c26efe9b4651c923bd8a01fd4
duoh <duoh@duohui-MacBookPro.local>
2021-12-13 11:06:49 +0900

    Updates

---
+++ precache-manifest.237a0f8d5f9c7cfc7670a71cbb841406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ab616e8f49d0e2db5e8fb103b5918c",
+    "url": "/jocafe/index.html"
+  },
+  {
+    "revision": "01ed554e6d5f60f19085",
+    "url": "/jocafe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "802742e571c8437a6ba2",
+    "url": "/jocafe/static/js/2.864e57d5.chunk.js"
+  },
+  {
+    "revision": "bc4662f59a7ac49af32ec4f291cf294c",
+    "url": "/jocafe/static/js/2.864e57d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ed554e6d5f60f19085",
+    "url": "/jocafe/static/js/main.5936eb27.chunk.js"
+  },
+  {
+    "revision": "e55bb00f2ce764481473",
+    "url": "/jocafe/static/js/runtime-main.6a7f444e.js"
+  }
+]);
